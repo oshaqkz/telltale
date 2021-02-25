682472ed31059ee8d15c1a31fd97054590107834
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2021-02-25 19:34:49 +0600

    Define custom env and make the project build

--- telltale/app/Main.hs
+++ telltale/app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import Lib
+import           ClassyPrelude
+import           Lib
 
 main :: IO ()
 main = someFunc
--- telltale/src/Foundation.hs
+++ telltale/src/Foundation.hs
@@ -2,11 +2,15 @@ module Foundation where
 
 import           ClassyPrelude
 
+data Config =
+  Config { _something :: Text
+         }
+
 newtype AppT m a =
-  AppT { runAppT :: ReaderT String m a
+  AppT { runAppT :: ReaderT Config m a
        } deriving ( Functor
                   , Applicative
                   , Monad
-                  , MonadReader String
+                  , MonadReader Config
                   )
 
