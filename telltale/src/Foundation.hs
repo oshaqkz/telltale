37ef4e3dc1d899e1a62ab0070a58121f638c0dc2
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2021-03-26 20:25:48 +0600

    Implement conventional runAppT function

--- telltale/src/Foundation.hs
+++ telltale/src/Foundation.hs
@@ -9,11 +9,14 @@ data Config =
 type App a = AppT IO a
 
 newtype AppT m a =
-  AppT { runAppT :: ReaderT Config m a
+  AppT { unAppT :: ReaderT Config m a
        } deriving ( Functor
                   , Applicative
                   , Monad
                   , MonadReader Config
                   , MonadIO
                   )
+
+runAppT :: Config -> AppT m a -> m a
+runAppT = flip $ runReaderT . unAppT
 
